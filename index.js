6dd583b0fee0f554aa594302fc0977046aff7cb8
Philip McMahon <philip.mcmahon@guardian.co.uk>
2016-04-18 15:14:42 +0100

    Implemented pagedGithubApiRequest function to deal with there being > 100 guardian github teams.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var AWS = require('aws-sdk');
 // (see https://github.com/orgs/guardian/teams)
 var TEAMS_TO_FETCH = ['Digital CMS', 'OpsManager-SSHAccess', 'Editorial Tools SSHAccess', 'Guardian Frontend', 'Discussion', 'Data Technology', 'Teamcity', 'Deploy Infrastructure', 'Membership and Subscriptions', 'Domains platform', 'Commercial dev', 'Content Platforms']
 
-function githubApiRequest(path) {
+function githubApiRequest(path, page) {
   return rp({
     uri: 'https://api.github.com' + path,
     headers: {
@@ -16,12 +16,28 @@ function githubApiRequest(path) {
       'User-Agent': 'nodey'
     },
     qs: {
-        per_page: 100
+        per_page: 100,
+        page: page
     },
     json: true
   }).catch(function(err) {
     console.error(err);
   });
+}
+
+function pagedGithubApiRequest(path, page, teamList) {
+    if (teamList) {
+        return githubApiRequest(path, page).then(function(resp) {
+            if (resp.length === 0) {
+                return teamList;
+            }
+            return pagedGithubApiRequest(path, page+1, teamList.concat(resp));
+            })
+    } else {
+        return githubApiRequest(path, page).then(function(resp){
+            return pagedGithubApiRequest(path, page+1, resp)
+        })
+    }
 }
 
 function membersListToLogin(membersList) {
@@ -98,7 +114,7 @@ function filterTeamList(teamList) {
 }
 
 exports.handler = function (event, context) {
-  var teamList = githubApiRequest('/orgs/guardian/teams').then(filterTeamList);
+  var teamList = pagedGithubApiRequest('/orgs/guardian/teams', 1, null).then(filterTeamList);
   var botList = getGithubBots();
   var teamsWithKeys = teamList.then(function(tl) {
     return botList.then(function(bl) {

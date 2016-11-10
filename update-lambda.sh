2e8c8cf0995e258d79e17403fdf3de3fb241e890
Joseph Smith <joseph.smith@guardian.co.uk>
2016-11-10 10:27:43 +0000

    Use Deploy Tools AWS account

--- update-lambda.sh
+++ update-lambda.sh
@@ -6,6 +6,6 @@ cp index.js index1.js
 zip -r -q keysToS3Lambda.zip index.js node_modules/ package.json
 rm index.js
 mv index1.js index.js
-aws s3 cp keysToS3Lambda.zip s3://lambda-dist-cross-stream/keys-to-s3/keysToS3Lambda.zip --profile composer
-aws lambda update-function-code --function-name keys-to-s3-lambda-KeysToS3Lambda-1LDJ9H96DQ5OA --s3-bucket lambda-dist-cross-stream --s3-key keys-to-s3/keysToS3Lambda.zip --profile composer
+aws s3 cp keysToS3Lambda.zip s3://deploy-tools-dist/deploy/PROD/keys-to-S3/keysToS3Lambda.zip --profile deployTools
+aws lambda update-function-code --function-name github-keys-to-s3 --s3-bucket deploy-tools-dist --s3-key deploy/PROD/keys-to-S3/keysToS3Lambda.zip --profile deployTools --region eu-west-1
 rm keysToS3Lambda.zip

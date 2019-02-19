9616cf5c6681e0210d5c8a00f8840dbc62c263dc
Philip McMahon <philip.mcmahon@guardian.co.uk>
2019-02-19 14:22:21 +0000

    Move to VPC, replace dynamo config with environment vars. Reduce no. teams for which keys are fetched.

--- index.js
+++ index.js
@@ -8,35 +8,13 @@ AWS.config.update({
   region: "eu-west-1"
 });
 
-var dynamoClient = new AWS.DynamoDB.DocumentClient();
-
 // add your github team name here to add your team's keys to the bucket
 // (see https://github.com/orgs/guardian/teams)
-var TEAMS_TO_FETCH = ['OpsManager-SSHAccess',
-                      'Guardian Frontend', 'Discussion', 'Data Technology', 'Teamcity',
+var TEAMS_TO_FETCH = ['Discussion', 'Data Technology', 'Teamcity',
                       'Deploy Infrastructure', 'Domains platform',
-                      'Commercial dev', 'Content Platforms', 'Multimedia', 'digital-department-website',
-                      'data science', 'Mobile Server-Side Staff', 'Identity', 'Identity SSH Access',
-                      "Guardian Frontend Team", 'Investigations SSH Access', 'Reader Revenue'];
+                      'Commercial dev', 'Multimedia', 'digital-department-website',
+                      'data science', 'Investigations SSH Access', 'Reader Revenue'];
 
-function configFromDynamo(functionName) {
-  var params = {
-    TableName: functionName + "-config"
-  };
-  return new Promise(function (fulfill, reject) {
-    dynamoClient.scan(params, function(err, data) {
-      if (err) reject(err)
-      else fulfill(data);
-    });
-  }).then(function (res) {
-    console.log(res.Items);
-    var config = {};
-    res.Items.forEach(function(row) {
-      config[row.key] = row.value
-    });
-    return config;
-  });
-}
 
 function githubApiRequest(path, githubOAuthToken, page) {
   return rp({
@@ -141,28 +119,26 @@ function filterTeamList(teamList) {
 }
 
 exports.handler = function (event, context) {
-  console.log('context:', context)
+  console.log('context:', context);
 
-  var functionName = context.functionName;
-  var configPromise = configFromDynamo(functionName);
+  const bucket = process.env.BUCKET;
+  const githubOauthToken = process.env.GITHUB_TOKEN;
 
-  configPromise.then(function (config) {
-    var botList = getGithubBots(config.githubOAuthToken);
-    var teams = pagedGithubApiRequest('/orgs/guardian/teams', config.githubOAuthToken, 1, null).then(filterTeamList);
+  var botList = getGithubBots(githubOauthToken);
+  var teams = pagedGithubApiRequest('/orgs/guardian/teams',githubOauthToken, 1, null).then(filterTeamList);
 
-    Promise.all([botList, teams]).then(function(bAndT) {
-      var bl = bAndT[0];
-      var tl = bAndT[1];
-      return tl.map(function(t) { return teamToTeamKeysObject(t, bl, config.githubOAuthToken);});
-    }).then(function(twk) {
-      console.log("There are " + twk.length + " teams to post: " + twk.map(function(team){return team.teamName;}));
-      twk.map(function(team) {
-        team.teamMembers.then(function(members) {
-          postToS3(config.bucket, team.teamName, members);
-        });
+  Promise.all([botList, teams]).then(function(bAndT) {
+    var bl = bAndT[0];
+    var tl = bAndT[1];
+    return tl.map(function(t) { return teamToTeamKeysObject(t, bl, githubOauthToken);});
+  }).then(function(twk) {
+    console.log("There are " + twk.length + " teams to post: " + twk.map(function(team){return team.teamName;}));
+    twk.map(function(team) {
+      team.teamMembers.then(function(members) {
+        postToS3(bucket, team.teamName, members);
       });
-    })
-  });
+    });
+  })
 };
 
 // For testing locally
